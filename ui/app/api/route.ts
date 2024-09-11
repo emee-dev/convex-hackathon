a81fa9ae9f6175fb69b97cc991fc4f2eee3a7adf
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2024-09-11 21:35:00 +0100

    fixing build error

--- ui/app/api/route.ts
+++ ui/app/api/route.ts
@@ -1,53 +1,53 @@
-// import { handleGetCLIUserProject } from "@/lib/action";
+import { FRONTEND_URL } from "@/const";
 // import { roles } from "@/types";
-// import { createClerkClient } from "@clerk/backend";
+import { createClerkClient } from "@clerk/backend";
 
-// export const FRONTEND_URL = process.env.FRONTEND_URL;
-
-// const clerkClient = createClerkClient({
-//   secretKey: process.env.CLERK_SECRET_KEY,
-// });
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 // Handle invitations
-// export const POST = async (req: Request) => {
-//   try {
-//     let params = (await req.json()) as { email: string; role_code: string };
+export const POST = async (req: Request) => {
+  try {
+    let params = (await req.json()) as { email: string; role_code: string };
 
-//     const response = await clerkClient.invitations.createInvitation({
-//       emailAddress: params.email,
-//       redirectUrl: `${FRONTEND_URL}/sign-up`,
-//       publicMetadata: {
-//         role: params.role_code,
-//       },
-//     });
+    const response = await clerkClient.invitations.createInvitation({
+      emailAddress: params.email,
+      redirectUrl: `${FRONTEND_URL}/sign-up`,
+      publicMetadata: {
+        role: params.role_code,
+      },
+    });
 
-//     if (response.status !== "pending" || "accepted") {
-//       return Response.json(
-//         {
-//           message: "Could not sent the invitation.",
-//           data: [],
-//         },
-//         {
-//           status: 500,
-//         }
-//       );
-//     }
+    if (response.status !== "pending" || "accepted") {
+      return Response.json(
+        {
+          message: "Could not sent the invitation.",
+          data: [],
+        },
+        {
+          status: 500,
+        }
+      );
+    }
 
-//     // return Response.json({ data: user.emailAddresses[0].emailAddress });
-//     return Response.json(
-//       {
-//         message: "An invitation was sent.",
-//         data: response,
-//       },
-//       { status: 200 }
-//     );
-//   } catch (error: any) {
-//     console.log("Error: ", error.message);
-//     return Response.json(
-//       { data: [] },
-//       {
-//         status: 500,
-//       }
-//     );
-//   }
-// };
+    // TODO use convex scheduling functions to schedule pending events.
+
+    // return Response.json({ data: user.emailAddresses[0].emailAddress });
+    return Response.json(
+      {
+        message: "An invitation was sent.",
+        data: response,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log("Error: ", error.message);
+    return Response.json(
+      { message: "Internal server error, please try again.", data: [] },
+      {
+        status: 500,
+      }
+    );
+  }
+};
